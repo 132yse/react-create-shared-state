c3623182d289ad440a717d393e735da17a962a27
伊撒尔 <1533540012@qq.com>
2019-11-26 22:46:25 +0800

    change to fre

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'fre'
 
-export function createSharedState<ValueType>(defaultValue: ValueType) {
-  type TListener = (value: ValueType) => void;
-
-  const listeners: Set<TListener> = new Set();
-  let backupValue = defaultValue;
+export function createContext(defaultValue) {
+  const listeners = new Set()
+  let backupValue = defaultValue
 
   return (): [ValueType, React.Dispatch<React.SetStateAction<ValueType>>] => {
     const [value, setValue] = useState<ValueType>(backupValue);
