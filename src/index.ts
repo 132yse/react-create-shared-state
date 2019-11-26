ccb8c567146f8c464ded7c2700d7f01c1ad275ca
伊撒尔 <1533540012@qq.com>
2019-11-26 22:48:04 +0800

    clean code

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,21 @@ export function createContext(defaultValue) {
   const listeners = new Set()
   let backupValue = defaultValue
 
-  return (): [ValueType, React.Dispatch<React.SetStateAction<ValueType>>] => {
-    const [value, setValue] = useState<ValueType>(backupValue);
+  return () => {
+    const [value, setValue] = useState(backupValue)
 
     useEffect(() => {
-      backupValue = value;
-      listeners.forEach(listener => listener !== setValue && listener(value));
-    }, [value]);
+      backupValue = value
+      listeners.forEach(listener => listener !== setValue && listener(value))
+    }, [value])
 
     useEffect(() => {
-      listeners.add(setValue);
+      listeners.add(setValue)
       return () => {
-        listeners.delete(setValue);
-      };
-    }, []);
+        listeners.delete(setValue)
+      }
+    }, [])
 
-    return [value, setValue];
-  };
+    return [value, setValue]
+  }
 }
